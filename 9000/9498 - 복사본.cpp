1d20db538118c00e1e397cd9d48bc0a2914ee4e7
wns5255 <44057297+wns5255@users.noreply.github.com>
2021-04-13 18:01:08 +0900

    test1

--- 9000/9498 - 복사본.cpp
+++ 9000/9498 - 복사본.cpp
@@ -7,7 +7,7 @@ int main(){
     if(-10000<=a&&a<=10000&&-10000<=b&&b<=10000)
     {
     if(a>b) cout << ">";
-    else if(a<=b) cout << "<";
+    else if(a<b) cout << "<";
     else cout << "==";
     }
     return 0;
